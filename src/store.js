523fd1a83958c2020088448e382df6352dbd9fc0
jeanCcana <jean_ccana@icloud.com>
2020-12-17 02:32:16 -0500

    images

--- src/store.js
+++ src/store.js
@@ -4,27 +4,28 @@ import empanadas from './img/Empanadas.jpg'
 import keke from './img/keke3.jpg'
 import pastel from './img/pastel.jpg'
 const initialState = {
-    productos: [{
-            name: 'ALFAJOR DE MAISENA - CAJA DE 25 ALFAJORES',
-            image: alfajor,
-            price: '5',
-        },
-        {
-            name: 'CAJA DE EMPANADAS',
-            image: empanadas,
-            price: '15.0',
-        },
-        {
-            name: 'KEKE DE CHOCOLATE',
-            image: keke,
-            price: '10.0',
-        },
-        {
-            name: 'PASTEL',
-            image: pastel,
-            price: '10.0',
-        },
-    ],
+  productos: [
+    {
+      name: 'ALFAJOR DE MAISENA - CAJA DE 25 ALFAJORES',
+      image: alfajor,
+      price: '5',
+    },
+    {
+      name: 'CAJA DE EMPANADAS',
+      image: empanadas,
+      price: '15.0',
+    },
+    {
+      name: 'KEKE DE CHOCOLATE',
+      image: keke,
+      price: '10.0',
+    },
+    {
+      name: 'PASTEL',
+      image: pastel,
+      price: '10.0',
+    },
+  ],
 }
 
 const reducer = (state = initialState, action) => state
